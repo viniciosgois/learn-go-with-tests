fe9486d23c51f81805951da329ea4bae4f74ec0e
Vinicios Gois <vinigadd@gmail.com>
2025-02-21 21:59:27 -0300

    array and slice

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -1,15 +1,37 @@
 package main
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	t.Run("should return the sum of slice with any size", func(t *testing.T) {
-		numbers := []int{1, 2, 3}
-		output := Sum(numbers)
-		expected := 6
+	input := []int{1, 2, 3}
+	output := Sum(input)
+	expected := 6
 
-		if output != expected {
-			t.Errorf("got %d expected %d given, %v", output, expected, numbers)
+	if output != expected {
+		t.Errorf("got %d expected %d given, %v", output, expected, input)
+	}
+}
+
+func TestSumAllTails(t *testing.T) {
+	checkSums := func(t testing.TB, got, expected []int) {
+		t.Helper()
+		if !slices.Equal(got, expected) {
+			t.Errorf(" got %v expected %v", got, expected)
 		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		expected := []int{2, 9}
+		checkSums(t, got, expected)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		expected := []int{0, 9}
+		checkSums(t, got, expected)
 	})
 }
