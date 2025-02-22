635c653a0c720321db4e1b590f7856bc4b200a32
Vinicios Gois <vinigadd@gmail.com>
2025-02-22 12:47:21 -0300

    structs types and interfaces

---
+++ structs/perimeter_test.go
@@ -0,0 +1,34 @@
+package structs
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
+	expected := 40.0
+
+	if got != expected {
+		t.Errorf("got %.2f expected %.2f", got, expected)
+	}
+}
+
+func TestArea(t *testing.T) {
+	areaTests := []struct {
+		name    string
+		shape   Shape
+		hasArea float64
+	}{
+		{name: "Rectangle", shape: Rectangle{Width: 12, Height: 6}, hasArea: 72.0},
+		{name: "Circle", shape: Circle{Radius: 10}, hasArea: 314.1592653589793},
+		{name: "Triangle", shape: Triangle{Base: 12, Height: 6}, hasArea: 36.0},
+	}
+
+	for _, tt := range areaTests {
+		t.Run(tt.name, func(t *testing.T) {
+			calculatedArea := tt.shape.Area()
+			if calculatedArea != tt.hasArea {
+				t.Errorf("%#v calculatedArea %g hasArea = %g", tt.shape, calculatedArea, tt.hasArea)
+			}
+		})
+	}
+}
